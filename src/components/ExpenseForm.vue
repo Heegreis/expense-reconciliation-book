c6da99da39ac5f1f977358bb5e419601d9648313
Heegreis <run3113214@gmail.com>
2023-07-01 17:31:16 +0800

    complete 2 layers of v-model structure of ExpenseForm

--- src/components/ExpenseForm.vue
+++ src/components/ExpenseForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import VueInputCalculator from 'vue-input-calculator'
 import db from 'components/db'
 
@@ -29,26 +29,27 @@ const props = defineProps({
 // 只用一個 prop data，拆成多個 Middle 餵給多個 input
 const emit = defineEmits(['update:modelValue'])
 
-const amountMiddle = computed({
-  get() {
-    return props.modelValue['amount']
-  },
-  set(newValue) {
-    const newModelValue = props.modelValue
-    newModelValue['amount'] = Number(newValue)
-    return emit('update:modelValue', newModelValue)
-  },
-})
-const dateMiddle = computed({
-  get() {
-    return props.modelValue['date']
-  },
-  set(newValue) {
-    const newModelValue = props.modelValue
-    newModelValue['date'] = newValue
-    return emit('update:modelValue', newModelValue)
-  },
-})
+function setMiddleValue(valueName) {
+  return computed({
+    get() {
+      return props.modelValue[valueName]
+    },
+    set(newValue) {
+      const newModelValue = props.modelValue
+      newModelValue[valueName] = Number(newValue)
+      return emit('update:modelValue', newModelValue)
+    },
+  })
+}
+
+const amountMiddle = setMiddleValue('amount')
+const dateMiddle = setMiddleValue('date')
+const categoryMiddle = setMiddleValue('category')
+const subCateMiddle = setMiddleValue('subCate')
+const accountMiddle = setMiddleValue('account')
+const remarkMiddle = setMiddleValue('remark')
+
+const options = ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle']
 </script>
 
 <template>
@@ -66,28 +67,28 @@ const dateMiddle = computed({
   </div>
   <div class="row">
     <div class="col">
-      <q-select v-model="model" :options="options" label="分類" />
+      <q-select v-model="categoryMiddle" :options="options" label="分類" />
       <!-- 要撈資料庫的選項，如果餵入''就去資料庫找預設選項 -->
       <!-- 但暫時沒資料庫，先完善下面的選項 -->
     </div>
   </div>
   <div class="row">
     <div class="col">
-      <q-select v-model="model" :options="options" label="子分類" />
+      <q-select v-model="subCateMiddle" :options="options" label="子分類" />
       <!-- 要撈資料庫的選項，如果餵入''就去資料庫找預設選項 -->
       <!-- 但暫時沒資料庫，先完善下面的選項 -->
     </div>
   </div>
   <div class="row">
     <div class="col">
-      <q-select v-model="model" :options="options" label="帳戶" />
+      <q-select v-model="accountMiddle" :options="options" label="帳戶" />
       <!-- 要撈資料庫的選項，如果餵入''就去資料庫找預設選項 -->
       <!-- 但暫時沒資料庫，先完善下面的選項 -->
     </div>
   </div>
   <div class="row">
     <div class="col">
-      <q-input v-model="text" autogrow label="備註"></q-input>
+      <q-input v-model="remarkMiddle" autogrow label="備註"></q-input>
     </div>
   </div>
   <!-- amount: 0, -->
